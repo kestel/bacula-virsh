f6d9dadb8b651e736c390ad2275d8dd5e2b6f393
Konstantin Shapkin <kestel@gmail.com>
2015-04-13 16:37:49 +0300

    Add several checks and improve markup.

--- before.sh
+++ before.sh
@@ -4,7 +4,8 @@
 export LC_ALL=en_GB.UTF-8 
 
 STORAGEPATH="/kvm"
-VIRSHPATH=`which virsh`
+#VIRSHPATH=`which virsh`
+VIRSHPATH="/usr/bin/virsh -d 4" 
 #VIRSHPATH=echo
 TIME=`date '+%d-%m-%Y_%H-%M'`
 YESTERDAY=`date -d yesterday '+%d-%m-%Y'`
@@ -26,98 +27,111 @@ else
 fi
 }
 
+check_for_blockjobs() {
+  if [ `$VIRSHPATH blockjob $VMNAME $DISK 2>/dev/null | grep -c "No current block job for $DISK"` -ne 1 ]
+  then
+    echo "ERROR: I've found active blockjobs for $VMNAME for disk $DISK, can't proceed. Check it manually"
+    exit 1
+  fi
+}
+
 if [ "$#" -lt 2 ]
 then
-	echo "Usage: $0 <vm name> <backup type> [<disk>] [<--no-quiesce>]"
-	exit 1
+  echo "Usage: $0 <vm name> <backup type> [<disk>] [<--no-quiesce>]"
+  exit 1
 else
-	VMNAME=$1
-	if [[ "$2" != "daily" && "$2" != "Incremental" && "$2" != "Differential" ]]
-	then 
-		BACKUPTYPE="Full"
-	elif [[ "$2" != "Full" && "$2" != "monthly" ]]
-	then
-		BACKUPTYPE="Incremental"
-	else
-		echo "<backup type> must be daily (Incremental or Differential) or Full (monthly)"
-		exit 1
-	fi
+  VMNAME=$1
+  if [[ "$2" != "daily" && "$2" != "Incremental" && "$2" != "Differential" ]]
+  then 
+    BACKUPTYPE="Full"
+  elif [[ "$2" != "Full" && "$2" != "monthly" ]]
+  then
+    BACKUPTYPE="Incremental"
+  else
+    echo "<backup type> must be daily (Incremental or Differential) or Full (monthly)"
+    exit 1
+  fi
 fi
 
-disk=${3-"vda"}
-quiesce="--quiesce"
-
-[ "$4" == "--no-quiesce" ] && quiesce=""
-
-if [ "$3" != "vda" ]
+if [ "$3" == "" ]
 then
-  DISK=$3
-else
   DISK="vda"
+else
+  DISK=$3
+fi
+
+if [ "$4" != "--no-quiesce" ]
+then
+  QUIESCE=""
+else
+  QUIESCE="--no-quiesce"
 fi
 
 # checking for Base snapshot
 if ! $VIRSHPATH snapshot-list $VMNAME | grep "base"
 then
-	echo "Error: can't find the base snapshot. It's required for this backup system. You can create it mannually:"
-	echo $VIRSHPATH snapshot-create-as $VMNAME base base
-	exit 1
+  echo "Error: can't find the base snapshot. It's required for this backup system. You can create it mannually:"
+  echo $VIRSHPATH snapshot-create-as $VMNAME base base
+  exit 1
 fi
 
 # if backup type is daily
 if [ $BACKUPTYPE = "Incremental" ]
 then
-# checking daily backup exist
-if [ `$VIRSHPATH snapshot-list $VMNAME | grep "daily" | wc -l` -ne 1 ]
-then
-	echo "$VMNAME has no daily backup, then I'll create it..."
-	if ! $VIRSHPATH snapshot-create-as --domain $VMNAME --name daily --description "Daily for $TIME" --disk-only --diskspec $disk,snapshot=external,file=$STORAGEPATH/$VMNAME-daily.qcow2 --atomic $quiesce; then
-		echo "Can't create snapshot. Error detected, exiting"
-		exit 1
-	fi
-	echo "Current snapshot doesn't exist for $VMNAME, let's create it"
-	if ! $VIRSHPATH snapshot-create-as --domain $VMNAME --name current --description "current on $TIME" --disk-only --diskspec $disk,snapshot=external,file=$STORAGEPATH/$VMNAME-current.qcow2 --atomic $quiesce; then
-		echo "Can't create snapshot. Error detected, exiting"
-		exit 1
-	fi
-else
-	echo "Daily backup found for $VMNAME"
-	if [ `$VIRSHPATH snapshot-list $VMNAME | grep "current" | wc -l` -eq 1 ]
-	then
-		echo "Current snapshot exist for $VMNAME, let's merge it into daily and create new"
-		if ! $VIRSHPATH blockcommit --domain $VMNAME $disk --base $STORAGEPATH/$VMNAME-daily.qcow2 --active --verbose --pivot; then
-			echo "Can't do blockcommit, error detected, exiting"
-			exit 1
-		fi
-		if [ `$VIRSHPATH domblklist $VMNAME | grep $disk | grep current | wc -l` -eq 0 ]
-		then
-			if [ -f $STORAGEPATH/$VMNAME-current.qcow2 ];
-			then
-				echo "removing old snapshot file"
-				remove_snapshot_file $STORAGEPATH/$VMNAME-current.qcow2
-				$VIRSHPATH snapshot-delete --domain $VMNAME current --metadata
-			fi
-			echo "Creating new current snapshot"
-			if ! $VIRSHPATH snapshot-create-as --domain $VMNAME --name current --description "Current for $TIME" --disk-only --diskspec $disk,snapshot=external,file=$STORAGEPATH/$VMNAME-current.qcow2 --atomic $quiesce; then
-				echo "Can't create snapshot"
-				exit 1
-			fi
-		fi
-	else
-		echo "Current snapshot doesn't exist for $VMNAME, let's create it"
-	        if ! $VIRSHPATH snapshot-create-as --domain $VMNAME --name current --description "Current on $TIME" --disk-only --diskspec $disk,snapshot=external,file=$STORAGEPATH/$VMNAME-current.qcow2 --atomic $quiesce; then
-			echo "Can't create snapshot. Exiting"
-			exit 1
-		fi
-	fi
-fi # fi for checking daily backup exist
+  check_for_blockjobs
+  # checking daily backup exist
+  if [ `$VIRSHPATH snapshot-list $VMNAME | grep "daily" | wc -l` -ne 1 ]
+  then
+  	echo "$VMNAME has no daily backup, then I'll create it..."
+  	if ! $VIRSHPATH snapshot-create-as --domain $VMNAME --name daily --description "Daily for $TIME" --disk-only --diskspec $DISK,snapshot=external,file=$STORAGEPATH/$VMNAME-daily.qcow2 --atomic $QUIESCE; then
+  		echo "Can't create snapshot. Error detected, exiting"
+  		exit 1
+  	fi
+  	echo "Current snapshot doesn't exist for $VMNAME, let's create it"
+  	if ! $VIRSHPATH snapshot-create-as --domain $VMNAME --name current --description "current on $TIME" --disk-only --diskspec $DISK,snapshot=external,file=$STORAGEPATH/$VMNAME-current.qcow2 --atomic $QUIESCE; then
+  		echo "Can't create snapshot. Error detected, exiting"
+  		exit 1
+  	fi
+  else
+  	check_for_blockjobs
+  	echo "Daily backup found for $VMNAME"
+  	if [ `$VIRSHPATH snapshot-list $VMNAME | grep "current" | wc -l` -eq 1 ]
+  	then
+  		echo "Current snapshot exist for $VMNAME, let's merge it into daily and create new"
+  		if ! $VIRSHPATH blockcommit --domain $VMNAME $DISK --base $STORAGEPATH/$VMNAME-daily.qcow2 --active --verbose --pivot; then
+  			echo "Can't do blockcommit, error detected, exiting"
+  			exit 1
+  		fi
+  		if [ `$VIRSHPATH domblklist $VMNAME | grep $DISK | grep current | wc -l` -eq 0 ]
+  		then
+  			if [ -f $STORAGEPATH/$VMNAME-current.qcow2 ];
+  			then
+  				echo "removing old snapshot file"
+  				remove_snapshot_file $STORAGEPATH/$VMNAME-current.qcow2
+  				$VIRSHPATH snapshot-delete --domain $VMNAME current --metadata
+  			fi
+  			echo "Creating new current snapshot"
+  			if ! $VIRSHPATH snapshot-create-as --domain $VMNAME --name current --description "Current for $TIME" --disk-only --diskspec $DISK,snapshot=external,file=$STORAGEPATH/$VMNAME-current.qcow2 --atomic $QUIESCE; then
+  				echo "Can't create snapshot"
+  				exit 1
+  			fi
+  		fi
+  	else
+  		echo "Current snapshot doesn't exist for $VMNAME, let's create it"
+  	        if ! $VIRSHPATH snapshot-create-as --domain $VMNAME --name current --description "Current on $TIME" --disk-only --diskspec $DISK,snapshot=external,file=$STORAGEPATH/$VMNAME-current.qcow2 --atomic $QUIESCE; then
+  			echo "Can't create snapshot. Exiting"
+  			exit 1
+  		fi
+  	fi
+  fi # fi for checking daily backup exist
 elif [ $BACKUPTYPE = "Full" ]
 then
+  check_for_blockjobs
 	echo "Full backup"
 	if [ `$VIRSHPATH snapshot-list $VMNAME | grep "current" | wc -l` -eq 1 ]
         then
 		echo "Current snapshot exist for $VMNAME, let's merge it into daily..."
-		if ! $VIRSHPATH blockcommit --domain $VMNAME $disk --base $STORAGEPATH/$VMNAME-daily.qcow2 --active --verbose --pivot; then
+		if ! $VIRSHPATH blockcommit --domain $VMNAME $DISK --base $STORAGEPATH/$VMNAME-daily.qcow2 --active --verbose --pivot; then
 			echo "Can't do blockcommit for current snapshot. Exiting"
 			exit 1
 		fi
@@ -130,7 +144,7 @@ then
 	if [ `$VIRSHPATH snapshot-list $VMNAME | grep "daily" | wc -l` -eq 1 ]
         then
 		echo "Daily snapshot exist for $VMNAME, let's merge it into base..."
-		if ! $VIRSHPATH blockcommit --domain $VMNAME $disk --base $STORAGEPATH/$VMNAME.qcow2 --active --verbose --pivot; then
+		if ! $VIRSHPATH blockcommit --domain $VMNAME $DISK --base $STORAGEPATH/$VMNAME.qcow2 --active --verbose --pivot; then
 			echo "Can't do blockcommit for current snapshot. Exiting"
 			exit 1
 		fi
@@ -141,12 +155,12 @@ then
 		remove_snapshot_file $STORAGEPATH/$VMNAME-daily.qcow2
 	fi
 	echo "Create new daily snapshot"
-        if ! $VIRSHPATH snapshot-create-as --domain $VMNAME --name daily --description "daily on $TODAY" --disk-only --diskspec $disk,snapshot=external,file=$STORAGEPATH/$VMNAME-daily.qcow2 --atomic $quiesce; then
+        if ! $VIRSHPATH snapshot-create-as --domain $VMNAME --name daily --description "daily on $TODAY" --disk-only --diskspec $DISK,snapshot=external,file=$STORAGEPATH/$VMNAME-daily.qcow2 --atomic $QUIESCE; then
 		echo "Can't create new daily snapshot. Exiting"
 		exit 1
 	fi
 	echo "Create new current snapshot"	
-	if ! $VIRSHPATH snapshot-create-as --domain $VMNAME --name current --description "current for $TIME" --disk-only --diskspec $disk,snapshot=external,file=$STORAGEPATH/$VMNAME-current.qcow2 --atomic $quiesce; then
+	if ! $VIRSHPATH snapshot-create-as --domain $VMNAME --name current --description "current for $TIME" --disk-only --diskspec $DISK,snapshot=external,file=$STORAGEPATH/$VMNAME-current.qcow2 --atomic $QUIESCE; then
 		echo "Can't create new current snapshot. Exiting"
 		exit 1
 	fi
